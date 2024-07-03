1274643edffbb50df49039a2f0854afe8160a66e
dominickasanga <dominickasanga@gmail.com>
2024-07-03 16:53:42 +0200

    modified:   index.html new file:   public/LIN-LOGO.png deleted:    public/favicon.ico odified:   src/App.vue modified:   src/components/HelloWorld.vue modified:   src/components/TheWelcome.vue

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -14,9 +14,9 @@ import SupportIcon from './icons/IconSupport.vue'
     </template>
     <template #heading>Documentation</template>
 
-    Vue’s
+    <!-- Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    provides you with all information you need to get started. -->
   </WelcomeItem>
 
   <WelcomeItem>
@@ -25,7 +25,7 @@ import SupportIcon from './icons/IconSupport.vue'
     </template>
     <template #heading>Tooling</template>
 
-    This project is served and bundled with
+    <!-- This project is served and bundled with
     <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
     recommended IDE setup is
     <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
@@ -34,11 +34,11 @@ import SupportIcon from './icons/IconSupport.vue'
     <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
     <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
       >Cypress Component Testing</a
-    >.
+    >. -->
 
-    <br />
+    <!-- <br />
 
-    More instructions are available in <code>README.md</code>.
+    More instructions are available in <code>README.md</code>. -->
   </WelcomeItem>
 
   <WelcomeItem>
@@ -47,14 +47,14 @@ import SupportIcon from './icons/IconSupport.vue'
     </template>
     <template #heading>Ecosystem</template>
 
-    Get official tools and libraries for your project:
+    <!-- Get official tools and libraries for your project:
     <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
     <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
     <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
     <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
     you need more resources, we suggest paying
     <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
+    a visit. -->
   </WelcomeItem>
 
   <WelcomeItem>
@@ -63,7 +63,7 @@ import SupportIcon from './icons/IconSupport.vue'
     </template>
     <template #heading>Community</template>
 
-    Got stuck? Ask your question on
+    <!-- Got stuck? Ask your question on
     <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
     Discord server, or
     <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
@@ -83,6 +83,6 @@ import SupportIcon from './icons/IconSupport.vue'
 
     As an independent project, Vue relies on community backing for its sustainability. You can help
     us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>. -->
   </WelcomeItem>
 </template>
